2b9f167cdaa85e532715f049945f9ea919fa5b3c
Milkriver <mapav4@yandex.ru>
2021-09-16 14:55:10 +0500

    addPost

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,11 +6,19 @@ const MyPosts = (props) => {
   let postsElements =
     props.posts.map(post => <Post message={post.message} likesNumber={post.likesNumber} />)
 
+let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
+
   return <div className={classes.myPosts}>
     <h3>My posts</h3>
     <div className={classes.newPost}>
-      <textarea></textarea></div>
-    <div><button>Add post</button></div>
+      <textarea ref={newPostElement}></textarea></div>
+    <div><button onClick={addPost}>Add post</button></div>
     {postsElements}
   </div>
 }
