37f27e701abf678a6ebdcba9412a833399f3dea6
Milkriver <mapav4@yandex.ru>
2021-09-19 22:47:43 +0500

    Верстка Post

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -5,8 +5,8 @@ const Post = (props) => {
   return <div className={classes.item} >
     <img className={classes.ava} src="https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg" />
     {props.message}
-    <div>
-      <span> Likes </span> <img className={classes.heart} src="https://img2.freepng.ru/20180604/ktp/kisspng-heart-clip-art-coracao-5b14e5f3d12471.3551228915280962438567.jpg" />
+    <div className={classes.likes}>
+      <span > Likes </span> <img className={classes.heart} src="https://img2.freepng.ru/20180604/ktp/kisspng-heart-clip-art-coracao-5b14e5f3d12471.3551228915280962438567.jpg" />
       {props.likesNumber}
 
     </div>
