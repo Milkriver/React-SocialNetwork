18def6da3e9c06716c6fc0a53b4800ebf342044a
Milkriver <mapav4@yandex.ru>
2021-10-16 22:46:58 +0500

    UsersReduser

--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -1,13 +1,13 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-        { id: 1, followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', country: 'Russia' } },
-        { id: 3, followed: false, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
+        // { id: 1, followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
+        // { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', country: 'Russia' } },
+        // { id: 3, followed: false, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
     ],
-    newPostText: ''
 };
 
 const UsersReducer = (state = initialState, action) => {
@@ -32,13 +32,16 @@ const UsersReducer = (state = initialState, action) => {
                     return u;
                 })
             }
-
+        case SET_USERS: {
+            return { ...state, users: [...state.users, ...action.users] };
+        }
         default:
             return state;
     }
 }
 
 export const followAC = (userId) => ({ type: FOLLOW, userId });
-export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
+export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
+export const setUsersAC = (users) => ({ type: SET_USERS, users });
 
 export default UsersReducer;
