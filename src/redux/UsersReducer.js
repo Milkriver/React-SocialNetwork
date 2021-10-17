38d968e61cda373275cd1892ba021d6f210df7c7
Milkriver <mapav4@yandex.ru>
2021-10-17 20:54:34 +0500

    Users

--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -4,9 +4,9 @@ const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-        // { id: 1, followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
-        // { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', country: 'Russia' } },
-        // { id: 3, followed: false, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
+        { id: 1, followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
+        { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', country: 'Russia' } },
+        { id: 3, followed: false, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
     ],
 };
 
