e3e46c151f1a51f1814a365b6743be85dc3db5bb
Milkriver <mapav4@yandex.ru>
2021-10-19 14:49:54 +0500

    Верстка блока Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,7 +3,30 @@ import React from 'react';
 
 let Users = (props) => {
     return <div>
-        USERS WILL BE HERE
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photoUrl} width={60}/>
+                    </div>
+                    <div>
+                        <button>Follow</button>
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.fullName}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{u.location.country}</div>
+                        <div>{u.location.city}</div>
+                    </span>
+
+                </span>
+            </div>)
+
+        }
     </div>
 }
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,12 @@ import { combineReducers, createStore } from "redux";
 import messagesPageReducer from "./messagesPageReducer";
 import profilePageReducer from "./profilePageReducer";
 import sidebarReducer from "./sidebarReducer";
+import UsersReducer from "./UsersReducer";
 
 let reducers = combineReducers({
-    profilePage: profilePageReducer, 
-    messagesPage: messagesPageReducer, 
- 
+    profilePage: profilePageReducer,
+    messagesPage: messagesPageReducer,
+    usersPage: UsersReducer,
 });
 
 let store = createStore(reducers);
